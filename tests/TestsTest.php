52c3854b44c8d3a8bcb76f095cfc928c7ecbf3af
MParzival <mick.aubin@gmail.com>
2019-12-19 12:06:26 +0100

    feat: PHPUnit

---
+++ tests/TestsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestsTest extends TestCase
+{
+
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+
+}
